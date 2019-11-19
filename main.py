1d31b3090a9da7d97fb85b434aa4820e0c55cbec
failedcoder12 <divyanshverma12@gmail.com>
2019-11-20 03:27:02 +0530

    Final

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def plot_graph(G):
 	plt.show()
 
 #Upto how much time stamp
-upto_tim = 30
+upto_tim = 100
 
 # Initialising a Graph
 G = nx.DiGraph()
@@ -56,7 +56,7 @@ G = nx.DiGraph()
 de = collections.deque([0,0])
 
 # Setting a minimum checking value for the 
-mini_check = 0.20
+mini_check = 0.05
 
 # Reading CSV File
 data = read_csv_file('input.csv')
@@ -64,146 +64,182 @@ data = read_csv_file('input.csv')
 # Compressing string to int
 dit = compressed_dictionaty(data,upto_tim)
 
-# Set p (Probability for selecting source node)
-p = 1
+def getting_warning_list(p=0.5,q=0.33,k=3):
 
-# Set q (Probability for selecting destination node)
-q = 1
+	graph_no_list = []
+	Gk = [0]*k
 
-#Setting number of nodes 
-k = 3
-Gk = [0]*k
+	#creating dequeue
+	de = [0]*k
 
-#creating dequeue
-de = [0]*k
+	#Setting number of previous dependents
+	prev_dependents = 3
 
-#Setting number of previous dependents
-prev_dependents = 3
-
-for i in range(k):
-	temp_lis = [0]*prev_dependents
-	de[i] = collections.deque(temp_lis)
-
-# Adding all nodes to the graph Plotting
-for x in dit:
-	G.add_node(dit[x])
-
-n = len(G)
-
-# Chosing source node list and destination list and creating K random graph
-sizes = [0]*k
-for i in range(k):
-	Gk[i] = []
-	source = [0]*n
-	dest = [0]*n
-	for x in G:
-		y = np.random.binomial(1, p)
-		if(y):
-			source[x] = 1
-		y = np.random.binomial(1, q)
-		if(y):
-			dest[x] = 1
-		Gk[i]=[source, dest]
-
-	for nodes in G:
-		if(source[nodes] or dest[nodes]):
-			sizes[i] = sizes[i] + 1
-
-prev = 0
-sum = 0
-sz = len(dit)
-
-def mean_sq_error(de):
-	mean_sq = 0
-
-	for itr in de:
-		mean_sq = mean_sq + (rat - itr)*(rat - itr)
-
-	mean_sq = math.sqrt(mean_sq)
-
-	if(mean_sq >= mini_check*sizes[i]):
-		return 1
-		
-	return 0
-
-def mean_ab_error(de):
-	mean_ab = 0
-
-	for itr in de:
-		mean_sq = mean_ab + abs(rat - itr)
-
-	if(mean_sq >= mini_check*sizes[i]):
-		return 1
-		
-	return 0
-
-for x in data:
-
-	if(int(x[0])>upto_tim):
-		break
-
-	# Taking 2 points connected by an edge
-	u = dit[x[1]]
-	v = dit[x[2]]
-	
-	sum = sum + 1
-	G.add_weighted_edges_from([(u,v,1)])
-	
-	# Adding all nodes of same timestamp
-	if(int(x[0]) == prev):
-		continue
-
-	prev = int(x[0])
-
-	flag = 0
-
-	error_list = [0]*k
-  
 	for i in range(k):
+		temp_lis = [0]*prev_dependents
+		de[i] = collections.deque(temp_lis)
 
-		if(sizes[i] == 0):
-			continue
-
-		source = Gk[i][0]
-		dest = Gk[i][1]
-		score = 0
-
-		# Making new subgraph
-		g = nx.DiGraph()
-
-		# Adding nodes in the graph
-		for nodes in G:
-			if(source[nodes] or dest[nodes]):
-				g.add_node(nodes)
-
-		# Creating subgraphs
-		for e in G.edges():
-			uu = e[0] 
-			vv = e[1]
-			if(source[uu] and dest[vv]):
-				g.add_weighted_edges_from([(uu,vv,1)])
-				score += 1
-
-		rat = score
-
-		if(mean_sq_error(de[i])):
-			flag = 1
-
-		error_list[i] = rat
-
-	plot_graph(G)
-
-	if(flag):
-		print("Warning")
-		print(error_list)
-	else:
-		for i in range(k):
-			de[i].append(error_list[i])
-			de[i].popleft()
-	
-	print(de)
-	
-	G.clear()
-
+	# Adding all nodes to the graph Plotting
 	for x in dit:
 		G.add_node(dit[x])
+
+	n = len(G)
+
+	# Chosing source node list and destination list and creating K random graph
+	sizes = [0]*k
+	for i in range(k):
+		Gk[i] = []
+		source = [0]*n
+		dest = [0]*n
+		for x in G:
+			y = np.random.binomial(1, p)
+			if(y):
+				source[x] = 1
+			y = np.random.binomial(1, q)
+			if(y):
+				dest[x] = 1
+			Gk[i]=[source, dest]
+
+		for nodes in G:
+			if(source[nodes] or dest[nodes]):
+				sizes[i] = sizes[i] + 1
+
+	prev = 0
+	sum = 0
+	sz = len(dit)
+
+	def mean_sq_error(de):
+		mean_sq = 0
+
+		for itr in de:
+			mean_sq = mean_sq + (rat - itr)*(rat - itr)
+
+		mean_sq = math.sqrt(mean_sq)
+
+		if(mean_sq >= mini_check*sizes[i]):
+			return 1
+			
+		return 0
+
+	def mean_ab_error(de):
+		mean_ab = 0
+
+		for itr in de:
+			mean_sq = mean_ab + abs(rat - itr)
+
+		if(mean_sq >= mini_check*sizes[i]):
+			return 1
+			
+		return 0
+
+	for x in data:
+
+		if(int(x[0])>upto_tim):
+			break
+
+		# Taking 2 points connected by an edge
+		u = dit[x[1]]
+		v = dit[x[2]]
+		
+		sum = sum + 1
+		G.add_weighted_edges_from([(u,v,1)])
+		
+		# Adding all nodes of same timestamp
+		if(int(x[0]) == prev):
+			continue
+
+		prev = int(x[0])
+
+		flag = 0
+
+		error_list = [0]*k
+	  
+		for i in range(k):
+
+			if(sizes[i] == 0):
+				continue
+
+			source = Gk[i][0]
+			dest = Gk[i][1]
+			score = 0
+
+			# Making new subgraph
+			g = nx.DiGraph()
+
+			# Adding nodes in the graph
+			for nodes in G:
+				if(source[nodes] or dest[nodes]):
+					g.add_node(nodes)
+
+			# Creating subgraphs
+			for e in G.edges():
+				uu = e[0] 
+				vv = e[1]
+				if(source[uu] and dest[vv]):
+					g.add_weighted_edges_from([(uu,vv,1)])
+					score += 1
+
+			rat = score
+
+			if(mean_sq_error(de[i])):
+				flag = 1
+
+			error_list[i] = rat
+
+		# plot_graph(G)
+
+		if(flag):
+			# print("Warning")
+			graph_no_list.append(int(x[0]))
+			# print(error_list)
+		else:
+			for i in range(k):
+				de[i].append(error_list[i])
+				de[i].popleft()
+		
+		# print(de)
+		
+		G.clear()
+
+		for x in dit:
+			G.add_node(dit[x])
+
+	return graph_no_list
+
+original_error_list = getting_warning_list(1,1,1)
+print(original_error_list)
+
+get_other_list1 = getting_warning_list(0.5,0.33,1)
+print(get_other_list1)
+
+get_other_list2 = getting_warning_list(0.5,0.33,20)
+print(get_other_list2)
+
+error1 = 0
+for i in original_error_list:
+	if(get_other_list1.count(i) == 0):
+		error1 = error1 + 1
+
+error2 = 0
+for i in original_error_list:
+	if(get_other_list2.count(i) == 0):
+		error2 = error2 + 1
+
+error1 = error1/len(original_error_list) * 100.00
+error2 = error2/len(original_error_list) * 100.00
+
+print(error1,error2)
+
+error1 = error1/len(original_error_list) * 100.00
+error2 = error2/len(original_error_list) * 100.00
+
+print(error1,error2)
+label = ['First','Second']
+index = np.arange(len(label))
+plt.bar([0,1], [error1,error2])
+plt.xlabel('Type')
+plt.ylabel('Error Percentage')
+plt.xticks(index, label)
+plt.title('Comparision')
+plt.show()
+# 100 ke liye 0.05
